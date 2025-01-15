d89e6bcf5cf5581cb267ce0ffe1715746d98b081
cat <kittynekostudio@gmail.com>
2025-01-14 17:25:25 -0800

    Created OpenGL CMake template for windows.

---
+++ vender/glfw-3.4/src/CMakeFiles/glfw.dir/win32_window.c.obj.d
@@ -0,0 +1,234 @@
+src/CMakeFiles/glfw.dir/win32_window.c.obj: \
+  C:/Users/eli12/dev/glfw-3.4/src/win32_window.c \
+  C:/Users/eli12/dev/glfw-3.4/src/internal.h \
+  C:/Users/eli12/dev/glfw-3.4/src/../include/GLFW/glfw3.h \
+  C:/llvm-mingw-64/lib/clang/19/include/stddef.h \
+  C:/llvm-mingw-64/include/stddef.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_header_macro.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_ptrdiff_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_size_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_wchar_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_null.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_offsetof.h \
+  C:/llvm-mingw-64/lib/clang/19/include/stdint.h \
+  C:/llvm-mingw-64/include/stdint.h C:/llvm-mingw-64/include/crtdefs.h \
+  C:/llvm-mingw-64/include/corecrt.h C:/llvm-mingw-64/include/_mingw.h \
+  C:/llvm-mingw-64/include/_mingw_mac.h \
+  C:/llvm-mingw-64/include/_mingw_secapi.h \
+  C:/llvm-mingw-64/lib/clang/19/include/vadefs.h \
+  C:/llvm-mingw-64/include/vadefs.h \
+  C:/llvm-mingw-64/include/sdks/_mingw_ddk.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_wint_t.h \
+  C:/Users/eli12/dev/glfw-3.4/src/platform.h \
+  C:/Users/eli12/dev/glfw-3.4/src/null_platform.h \
+  C:/Users/eli12/dev/glfw-3.4/src/win32_platform.h \
+  C:/llvm-mingw-64/include/wctype.h C:/llvm-mingw-64/include/windows.h \
+  C:/llvm-mingw-64/include/sdkddkver.h C:/llvm-mingw-64/include/excpt.h \
+  C:/llvm-mingw-64/lib/clang/19/include/stdarg.h \
+  C:/llvm-mingw-64/include/stdarg.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg_header_macro.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg___gnuc_va_list.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg_va_list.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg_va_arg.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg___va_copy.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stdarg_va_copy.h \
+  C:/llvm-mingw-64/include/windef.h \
+  C:/llvm-mingw-64/include/winapifamily.h \
+  C:/llvm-mingw-64/include/minwindef.h \
+  C:/llvm-mingw-64/include/specstrings.h C:/llvm-mingw-64/include/sal.h \
+  C:/llvm-mingw-64/include/concurrencysal.h \
+  C:/llvm-mingw-64/include/driverspecs.h \
+  C:/llvm-mingw-64/include/winnt.h \
+  C:/llvm-mingw-64/include/_mingw_unicode.h \
+  C:/llvm-mingw-64/include/ctype.h C:/llvm-mingw-64/include/apiset.h \
+  C:/llvm-mingw-64/include/psdk_inc/intrin-impl.h \
+  C:/llvm-mingw-64/include/basetsd.h C:/llvm-mingw-64/include/guiddef.h \
+  C:/llvm-mingw-64/include/string.h \
+  C:/llvm-mingw-64/include/sec_api/string_s.h \
+  C:/llvm-mingw-64/lib/clang/19/include/x86intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/ia32intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/immintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/x86gprintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/hresetintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/uintrintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/usermsrintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/crc32intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/prfchiintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/raointintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/cmpccxaddintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/mmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xmmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/mm_malloc.h \
+  C:/llvm-mingw-64/include/stdlib.h \
+  C:/llvm-mingw-64/include/corecrt_wstdlib.h \
+  C:/llvm-mingw-64/lib/clang/19/include/limits.h \
+  C:/llvm-mingw-64/include/limits.h \
+  C:/llvm-mingw-64/include/sec_api/stdlib_s.h \
+  C:/llvm-mingw-64/include/malloc.h \
+  C:/llvm-mingw-64/lib/clang/19/include/emmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/pmmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/tmmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/smmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/popcntintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/wmmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__wmmintrin_aes.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__wmmintrin_pclmul.h \
+  C:/llvm-mingw-64/lib/clang/19/include/clflushoptintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/clwbintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx2intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/f16cintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/bmiintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/bmi2intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/lzcntintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/fmaintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512fintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512bwintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512bitalgintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512cdintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vpopcntdqintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vpopcntdqvlintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vnniintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlvnniintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxvnniintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512dqintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlbitalgintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlbwintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlcdintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vldqintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512ifmaintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512ifmavlintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxifmaintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vbmiintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vbmivlintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vbmi2intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlvbmi2intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512fp16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlfp16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512bf16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlbf16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/pkuintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/vpclmulqdqintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/vaesintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/gfniintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxvnniint8intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxneconvertintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/sha512intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/sm3intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/sm4intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avxvnniint16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/rtmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xtestintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/shaintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/fxsrintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xsaveintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xsaveoptintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xsavecintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xsavesintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/cetintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/adcintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/adxintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/rdseedintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/wbnoinvdintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/cldemoteintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/waitpkgintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/movdirintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/pconfigintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/sgxintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/ptwriteintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/invpcidintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/amxfp16intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/keylockerintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/amxintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/amxcomplexintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vp2intersectintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/avx512vlvp2intersectintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/enqcmdintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/serializeintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/tsxldtrkintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/prfchwintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/ammintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/fma4intrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/xopintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/tbmintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/lwpintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/mwaitxintrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/clzerointrin.h \
+  C:/llvm-mingw-64/lib/clang/19/include/rdpruintrin.h \
+  C:/llvm-mingw-64/include/pshpack4.h C:/llvm-mingw-64/include/poppack.h \
+  C:/llvm-mingw-64/include/pshpack2.h \
+  C:/llvm-mingw-64/include/pshpack8.h \
+  C:/llvm-mingw-64/include/ktmtypes.h C:/llvm-mingw-64/include/winbase.h \
+  C:/llvm-mingw-64/include/apisetcconv.h \
+  C:/llvm-mingw-64/include/minwinbase.h \
+  C:/llvm-mingw-64/include/bemapiset.h \
+  C:/llvm-mingw-64/include/debugapi.h \
+  C:/llvm-mingw-64/include/errhandlingapi.h \
+  C:/llvm-mingw-64/include/fibersapi.h \
+  C:/llvm-mingw-64/include/fileapi.h \
+  C:/llvm-mingw-64/include/handleapi.h \
+  C:/llvm-mingw-64/include/heapapi.h C:/llvm-mingw-64/include/ioapiset.h \
+  C:/llvm-mingw-64/include/interlockedapi.h \
+  C:/llvm-mingw-64/include/jobapi.h \
+  C:/llvm-mingw-64/include/libloaderapi.h \
+  C:/llvm-mingw-64/include/memoryapi.h \
+  C:/llvm-mingw-64/include/namedpipeapi.h \
+  C:/llvm-mingw-64/include/namespaceapi.h \
+  C:/llvm-mingw-64/include/processenv.h \
+  C:/llvm-mingw-64/include/processthreadsapi.h \
+  C:/llvm-mingw-64/include/processtopologyapi.h \
+  C:/llvm-mingw-64/include/profileapi.h \
+  C:/llvm-mingw-64/include/realtimeapiset.h \
+  C:/llvm-mingw-64/include/securityappcontainer.h \
+  C:/llvm-mingw-64/include/securitybaseapi.h \
+  C:/llvm-mingw-64/include/synchapi.h \
+  C:/llvm-mingw-64/include/sysinfoapi.h \
+  C:/llvm-mingw-64/include/systemtopologyapi.h \
+  C:/llvm-mingw-64/include/threadpoolapiset.h \
+  C:/llvm-mingw-64/include/threadpoollegacyapiset.h \
+  C:/llvm-mingw-64/include/utilapiset.h \
+  C:/llvm-mingw-64/include/wow64apiset.h \
+  C:/llvm-mingw-64/include/winerror.h \
+  C:/llvm-mingw-64/include/fltwinerror.h \
+  C:/llvm-mingw-64/include/timezoneapi.h \
+  C:/llvm-mingw-64/include/wingdi.h C:/llvm-mingw-64/include/pshpack1.h \
+  C:/llvm-mingw-64/include/winuser.h C:/llvm-mingw-64/include/tvout.h \
+  C:/llvm-mingw-64/include/winnls.h \
+  C:/llvm-mingw-64/include/datetimeapi.h \
+  C:/llvm-mingw-64/include/stringapiset.h \
+  C:/llvm-mingw-64/include/wincon.h \
+  C:/llvm-mingw-64/include/wincontypes.h \
+  C:/llvm-mingw-64/include/consoleapi.h \
+  C:/llvm-mingw-64/include/consoleapi2.h \
+  C:/llvm-mingw-64/include/consoleapi3.h \
+  C:/llvm-mingw-64/include/winver.h C:/llvm-mingw-64/include/winreg.h \
+  C:/llvm-mingw-64/include/reason.h C:/llvm-mingw-64/include/winnetwk.h \
+  C:/llvm-mingw-64/include/wnnc.h C:/llvm-mingw-64/include/virtdisk.h \
+  C:/llvm-mingw-64/include/stralign.h \
+  C:/llvm-mingw-64/include/sec_api/stralign_s.h \
+  C:/llvm-mingw-64/include/winsvc.h C:/llvm-mingw-64/include/mcx.h \
+  C:/llvm-mingw-64/include/imm.h C:/llvm-mingw-64/include/dinput.h \
+  C:/llvm-mingw-64/include/objbase.h C:/llvm-mingw-64/include/rpc.h \
+  C:/llvm-mingw-64/include/rpcdce.h C:/llvm-mingw-64/include/rpcdcep.h \
+  C:/llvm-mingw-64/include/rpcnsi.h C:/llvm-mingw-64/include/rpcnterr.h \
+  C:/llvm-mingw-64/include/rpcasync.h C:/llvm-mingw-64/include/rpcndr.h \
+  C:/llvm-mingw-64/include/rpcnsip.h C:/llvm-mingw-64/include/rpcsal.h \
+  C:/llvm-mingw-64/include/combaseapi.h \
+  C:/llvm-mingw-64/include/wtypesbase.h \
+  C:/llvm-mingw-64/include/unknwnbase.h \
+  C:/llvm-mingw-64/include/objidlbase.h C:/llvm-mingw-64/include/cguid.h \
+  C:/llvm-mingw-64/include/objidl.h C:/llvm-mingw-64/include/unknwn.h \
+  C:/llvm-mingw-64/include/wtypes.h C:/llvm-mingw-64/include/urlmon.h \
+  C:/llvm-mingw-64/include/oleidl.h C:/llvm-mingw-64/include/servprov.h \
+  C:/llvm-mingw-64/include/msxml.h C:/llvm-mingw-64/include/oaidl.h \
+  C:/llvm-mingw-64/include/propidl.h \
+  C:/llvm-mingw-64/include/_mingw_dxhelper.h \
+  C:/llvm-mingw-64/include/xinput.h C:/llvm-mingw-64/include/dbt.h \
+  C:/Users/eli12/dev/glfw-3.4/src/null_joystick.h \
+  C:/Users/eli12/dev/glfw-3.4/src/win32_joystick.h \
+  C:/Users/eli12/dev/glfw-3.4/src/win32_thread.h \
+  C:/Users/eli12/dev/glfw-3.4/src/win32_time.h \
+  C:/Users/eli12/dev/glfw-3.4/src/../include/GLFW/glfw3native.h \
+  C:/llvm-mingw-64/include/windowsx.h \
+  C:/llvm-mingw-64/include/shellapi.h
---
+++ vender/glfw-3.4/tests/CMakeFiles/reopen.dir/reopen.c.obj.d
@@ -0,0 +1,41 @@
+tests/CMakeFiles/reopen.dir/reopen.c.obj: \
+  C:/Users/eli12/dev/glfw-3.4/tests/reopen.c \
+  C:/Users/eli12/dev/glfw-3.4/deps/glad/gl.h \
+  C:/llvm-mingw-64/lib/clang/19/include/stdint.h \
+  C:/llvm-mingw-64/include/stdint.h C:/llvm-mingw-64/include/crtdefs.h \
+  C:/llvm-mingw-64/include/corecrt.h C:/llvm-mingw-64/include/_mingw.h \
+  C:/llvm-mingw-64/include/_mingw_mac.h \
+  C:/llvm-mingw-64/include/_mingw_secapi.h \
+  C:/llvm-mingw-64/lib/clang/19/include/vadefs.h \
+  C:/llvm-mingw-64/include/vadefs.h \
+  C:/llvm-mingw-64/include/sdks/_mingw_ddk.h \
+  C:/llvm-mingw-64/lib/clang/19/include/stddef.h \
+  C:/llvm-mingw-64/include/stddef.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_wchar_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_wint_t.h \
+  C:/llvm-mingw-64/include/stdio.h \
+  C:/llvm-mingw-64/include/corecrt_stdio_config.h \
+  C:/llvm-mingw-64/include/_mingw_off_t.h \
+  C:/llvm-mingw-64/include/swprintf.inl \
+  C:/llvm-mingw-64/include/sec_api/stdio_s.h \
+  C:/llvm-mingw-64/include/stdlib.h \
+  C:/llvm-mingw-64/include/corecrt_wstdlib.h \
+  C:/llvm-mingw-64/lib/clang/19/include/limits.h \
+  C:/llvm-mingw-64/include/limits.h \
+  C:/llvm-mingw-64/include/sec_api/stdlib_s.h \
+  C:/llvm-mingw-64/include/malloc.h \
+  C:/llvm-mingw-64/lib/clang/19/include/mm_malloc.h \
+  C:/llvm-mingw-64/include/string.h \
+  C:/llvm-mingw-64/include/sec_api/string_s.h \
+  C:/Users/eli12/dev/glfw-3.4/include/GLFW/glfw3.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_header_macro.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_ptrdiff_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_size_t.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_null.h \
+  C:/llvm-mingw-64/lib/clang/19/include/__stddef_offsetof.h \
+  C:/llvm-mingw-64/include/time.h C:/llvm-mingw-64/include/sys/timeb.h \
+  C:/llvm-mingw-64/include/sec_api/sys/timeb_s.h \
+  C:/llvm-mingw-64/include/_timeval.h \
+  C:/llvm-mingw-64/include/pthread_time.h \
+  C:/Users/eli12/dev/glfw-3.4/deps/linmath.h \
+  C:/llvm-mingw-64/include/math.h
